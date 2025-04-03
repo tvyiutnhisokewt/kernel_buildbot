0bc0f7288b7817432e7e5db1cdbd33d15853e20c
[kernel buildbot] vy <vy@buildbot.tvyiutnhisokewt>
2025-04-03 11:05:37 +0000

    [kernel buildbot] tvy: upload prebuilt to branch
    Signed-off-by: [kernel buildbot] vy <vy@buildbot.tvyiutnhisokewt>

---
+++ dist/kernel-headers/include/generated/compile.h
@@ -0,0 +1,4 @@
+#define UTS_MACHINE		"aarch64"
+#define LINUX_COMPILE_BY	"build-user"
+#define LINUX_COMPILE_HOST	"build-host"
+#define LINUX_COMPILER		"Android (10087095, +pgo, +bolt, +lto, -mlgo, based on r487747c) clang version 17.0.2 (https://android.googlesource.com/toolchain/llvm-project d9f89f4d16663d5012e5c09495f3b30ece3d2362), LLD 17.0.2"
---
+++ dist/kernel-headers/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "6.1.99-android14-11-g73114a4eb13e"
